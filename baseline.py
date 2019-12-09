edadc36c7baae12a2e8d65ef13b0a8a2cbed3391
Andrew Deng <dsyandrewdeng@gmail.com>
2019-12-09 09:54:47 -0800

    add gcn to baseline

--- baseline.py
+++ baseline.py
@@ -5,6 +5,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.metrics import confusion_matrix
 import data_loader
+import torch_geometric
+import torch_geometric.nn as pyg_nn
+
+class BaselineGCN(nn.Module):
+    def __init__(self, input_dim, model):
+        super(BaselineGCN, self).__init__()
+        self.conv = pyg_nn.GCNConv(in_channels = input_dim, out_channels = input_dim)
+        self.post_model = model
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv(x, edge_index)
+        
+        return self.post_model(x)
 
 class SimpleNeuralNetwork(nn.Module):
     def __init__(self, input_dim, output_dim, num_hidden_layers, hidden_dim):
@@ -20,6 +35,32 @@ class SimpleNeuralNetwork(nn.Module):
         for layer in self.layers:
             x = layer(x)
         return F.log_softmax(x, dim=1)
+
+# Need to feed in same gnn data
+def train_gcn_baseline(gds, output_dim, num_hidden_layers, num_epochs, hidden_dim = None):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    input_dim = gds[0].tg_graph['x'].shape[1]
+    linear_model = SimpleNeuralNetwork(input_dim, output_dim, num_hidden_layers, hidden_dim).to(device)
+    model = BaselineGCN(input_dim, linear_model).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+    traffic_classes = torch.cat([torch.tensor(gd.traffic_classes, device=device).to(torch.int64) for gd in gds], dim=0)
+    tg_graphs = torch_geometric.data.Batch.from_data_list([gd.tg_graph for gd in gds]).to(device)
+    class_weights = torch.stack([torch.tensor(gd.class_weights, device=device) for gd in gds]).sum(dim=0)
+
+    model.train()
+    for epoch in range(num_epochs):
+        optimizer.zero_grad()
+        out = model(tg_graphs)
+        #loss = F.mse_loss(out, traffic_values)
+        loss = F.nll_loss(out, traffic_classes, weight=class_weights)
+        accuracy = (out.argmax(dim=1)==traffic_classes).float().mean()
+        print('Epoch %03d: loss = %.5f, accuracy = %.5f' % (epoch, loss, accuracy))
+        loss.backward()
+        optimizer.step()
+    # Return model in eval mode
+    model.eval()
+    return model
 
 def train_neural_network(data, output_dim, num_hidden_layers, num_epochs, hidden_dim=None):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -73,4 +114,25 @@ def plot_map_with_neural_network_predicted_traffic(data, model):
     osmid_to_idx = {x:i for i,x in enumerate(linear_data.index)}
     for v1, v2, edge in data.graph.edges(data=True):
         edge['traffic_class_predicted'] = int(out[osmid_to_idx[edge['osmid']]])
+    data_loader.plot_map_with_traffic(data.graph, 'traffic_class_predicted')
+
+
+def plot_map_with_gnn_predicted_traffic_gcn(data, gd, model):
+    # max(dim=1) returns values, indices tuple; only need indices
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    tg_graph = gd.tg_graph.to(device)
+    traffic_classes = torch.tensor(gd.traffic_classes, device=device)
+    out = model(tg_graph).detach().max(dim=1)[1]
+    print('Accuracy =', float((out==traffic_classes).to(torch.float64).mean()))
+    traffic_classes = traffic_classes.cpu()
+    out = out.cpu()
+    cm = confusion_matrix(traffic_classes, out)
+    print('Accuracy confusion matrix\n', cm)
+    per_class_accuracy = cm[np.arange(4), np.arange(4)]/np.sum(cm, axis=1)
+    print('Per-class accuracy:', per_class_accuracy.mean(), per_class_accuracy)
+    for v1, v2, edge in data.graph.edges(data=True):
+        # line_graph will use vertices (a1,a2) where a1<a2
+        a1, a2 = v1, v2
+        if a1 > a2: a1, a2 = a2, a1
+        edge['traffic_class_predicted'] = int(out[gd.node_map[a1,a2]])
     data_loader.plot_map_with_traffic(data.graph, 'traffic_class_predicted')
